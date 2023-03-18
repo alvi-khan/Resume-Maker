3bbc6f0158a52f142ecb8f67d56c469bcbc1c8e8
Ridwan230 <191.ridwan@gmail.com>
2023-03-19 01:53:51 +0600

    Login page UI changed

--- lib/components/login_signup.dart
+++ lib/components/login_signup.dart
@@ -9,7 +9,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   @override
   Widget build(BuildContext context) {
     TextEditingController _emailController = TextEditingController();
@@ -17,94 +16,91 @@ class _LoginPageState extends State<LoginPage> {
 
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Resume Creator",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 44.0,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30.0,
             ),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          const Text(
-            "Login",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          TextField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: "User Email",
-              prefixIcon: Icon(Icons.mail, color: Colors.black),
-            ),
-          ),
-          const SizedBox(
-            height: 26.0,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: "User Password",
-              prefixIcon: Icon(Icons.lock, color: Colors.black),
-            ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text(
-            "Don't Remember your Password?",
-            style: TextStyle(color: Colors.blue),
-          ),
-          const SizedBox(
-            height: 88.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: Button(
-              onPressed: () => Authentication.loginUsingEmailPassword(
-                  email: _emailController.text,
-                  password: _passwordController.text
+            const Center(
+              child: Text(
+                "Resume Creator",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 44.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              text: "Login",
             ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: Button(
-              onPressed: () => Authentication.signInWithGoogle(),
-              text: "Google Login",
+            const SizedBox(
+              height: 15.0,
             ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: Button(
-              onPressed: () => Authentication.createUser(
-                  email: _emailController.text,
-                  password: _passwordController.text
+            Center(
+              child: Image.asset(
+                'assets/images/login.png',
+                height: 300,
               ),
-              text: "Sign Up",
             ),
-          )
-        ],
+            const SizedBox(
+              height: 15.0,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "User Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: Button(
+                onPressed: () => Authentication.loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text),
+                text: "Login",
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: Button(
+                onPressed: () => Authentication.signInWithGoogle(),
+                text: "Log in with Google",
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: Button(
+                onPressed: () => Authentication.createUser(
+                    email: _emailController.text,
+                    password: _passwordController.text),
+                text: "Sign Up",
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -124,16 +120,13 @@ class Button extends StatelessWidget {
           backgroundColor: const Color(0xFF0069FE),
           foregroundColor: Colors.white,
           padding: const EdgeInsets.symmetric(vertical: 20.0),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0)
-          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
         ),
         child: Text(text,
             style: const TextStyle(
               color: Colors.white,
               fontSize: 18.0,
-            )
-        )
-    );
+            )));
   }
 }
